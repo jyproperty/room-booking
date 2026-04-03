e664a47026df9a49bb155a5dae0e6b0c4d14065c
jyproperty <jyproperty@users.noreply.github.com>
2026-04-03 16:34:33 +0900

    fix: HashRouter로 변경 (GitHub Pages 호환)

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import { Box } from '@mui/material'
 import Navbar from './components/Navbar'
 import HomePage from './pages/HomePage'
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Box sx={{ bgcolor: 'background.default', minHeight: '100vh' }}>
         <Navbar />
         <Routes>
           <Route path="/" element={<HomePage />} />
         </Routes>
       </Box>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
